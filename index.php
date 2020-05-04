fb2d5fa0eb359db59364b1c38b5593d127875a85
yahakim19 <yusufhakim419@gmail.com>
2020-05-05 01:36:56 +0700

    update link

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,10 +13,11 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <!-- Social Button Library -->
-    
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
+
 <body>
 
     <header>
@@ -23,11 +25,10 @@
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">Artics Pictures</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-        
+
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto ">
                     <li class="nav-item active">
@@ -50,23 +51,23 @@
                 <div class="row float-right" style="margin-right: 10px;">
 
                     <a href="#" class="fa fa-pinterest" style="margin-right: 10px;"></a>
-                    <a href="#" class="fa fa-instagram" style="margin-right: 10px;"></a>
+                    <a href="https://www.instagram.com/articspictures/" class="fa fa-instagram" style="margin-right: 10px;"></a>
 
                 </div>
 
-                
+
 
 
             </div>
 
-            
+
         </nav>
         <!-- navbar end -->
 
     </header>
 
     <!-- Promotion and Update -->
-    <section class="container col-md-11" style="margin-top: 10px;" >
+    <section class="container col-md-11" style="margin-top: 10px;">
         <!-- <div class="jumbotron">
             <h1 class="display-4">Hello, world!</h1>
             <p class="lead">We`ll shot your best  moment</p>
@@ -93,129 +94,128 @@
 
     <!-- Choose Packages -->
 
-    
-
-        <section class="container border" style="padding: 10px;">
-
-            <h3 class="text-center display-4" style="margin: 10px 0 15px 0;"> Packages  </h3>
-
-        
-            <div class="row">
-                
-                
-                    <div class="col-sm-6">
-                        <div class="card"   >
-                            <img class="card-img-top img-thumbnail" style="height: 300px;" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Graduation image cap">
-                            <div class="card-body">
-                                <h5 class="card-title text-center">Graduation</h5>
-                                <p class="card-text">
-                                    <ul>
-                                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                                        <li>Praesent consectetur sapien sit amet egestas ultrices</li>
-                                        <li>In eleifend nisl id ultricies sodales</li>
-                                    </ul>
-                                </p>
-                                <!-- <a href="#" class="btn btn-primary float-right">Book Now</a> -->
-                                <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target=".bd-example-modal-sm">Book Now</button>
-                                
-                                <div class="modal fade bd-example-modal-sm " tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel"
-                                    aria-hidden="true">
-                                    <div class="modal-dialog modal-sm">
-                                        <div class="modal-content">
-                                            ...
-                                            <!-- Form Book -->
-                                
-                                
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="card"   >
-                            <img class="card-img-top img-thumbnail" style="height: 300px;" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Prewedding image cap">
-                            <div class="card-body">
-                                <h5 class="card-title text-center">Pre Wedding</h5>
-                                <p class="card-text">
-                                    <ul>
-                                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                                        <li>Praesent consectetur sapien sit amet egestas ultrices</li>
-                                        <li>In eleifend nisl id ultricies sodales</li>
-                                    </ul>
-                                </p>
-                                <!-- <a href="#" class="btn btn-primary float-right">Book Now</a> -->
-                                <!-- Small modal -->
-                                <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target=".bd-example-modal-sm">Book Now</button>
-                                
-                                <div class="modal fade bd-example-modal-sm " tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog modal-sm">
-                                        <div class="modal-content">
-                                            ...
-                                            <!-- Form Book -->
 
 
-                                        </div>
-                                    </div>
+    <section class="container border" style="padding: 10px;">
+
+        <h3 class="text-center display-4" style="margin: 10px 0 15px 0;"> Packages </h3>
+
+
+        <div class="row">
+
+
+            <div class="col-sm-6">
+                <div class="card">
+                    <img class="card-img-top img-thumbnail" style="height: 300px;" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Graduation image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Graduation</h5>
+                        <p class="card-text">
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                                <li>Praesent consectetur sapien sit amet egestas ultrices</li>
+                                <li>In eleifend nisl id ultricies sodales</li>
+                            </ul>
+                        </p>
+                        <!-- <a href="#" class="btn btn-primary float-right">Book Now</a> -->
+                        <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target=".bd-example-modal-sm">Book Now</button>
+
+                        <div class="modal fade bd-example-modal-sm " tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-sm">
+                                <div class="modal-content">
+                                    ...
+                                    <!-- Form Book -->
+
+
                                 </div>
                             </div>
                         </div>
+
                     </div>
-       
+                </div>
             </div>
-        </section>
+            <div class="col-sm-6">
+                <div class="card">
+                    <img class="card-img-top img-thumbnail" style="height: 300px;" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Prewedding image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Pre Wedding</h5>
+                        <p class="card-text">
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                                <li>Praesent consectetur sapien sit amet egestas ultrices</li>
+                                <li>In eleifend nisl id ultricies sodales</li>
+                            </ul>
+                        </p>
+                        <!-- <a href="#" class="btn btn-primary float-right">Book Now</a> -->
+                        <!-- Small modal -->
+                        <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target=".bd-example-modal-sm">Book Now</button>
+
+                        <div class="modal fade bd-example-modal-sm " tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-sm">
+                                <div class="modal-content">
+                                    ...
+                                    <!-- Form Book -->
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
     <!-- Choose Packages End -->
 
-    
+
 
     <!-- Others -->
-        <section class="container card" style="margin-top: 30px;padding: 30px;">
-             <div class="row featurette">
-                 <div class="col-md-7">
-                    <h2 class="featurette-heading">Custom Packages</h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-                </div>
-                
-                    <div class="col-md-5">
-                        <img class="featurette-image img-fluid mx-auto" data-src="" alt="Generic placeholder image">
-                    </div>
-                </div>
+    <section class="container card" style="margin-top: 30px;padding: 30px;">
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">Custom Packages</h2>
+                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </div>
 
-                <!-- Small modal -->
-                <button type="button"  class="btn btn-primary col-sm-6" data-toggle="modal" data-target=".bd-example-modal-sm">Custom</button>
-                
-                <div class="modal fade bd-example-modal-sm " tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel"
-                    aria-hidden="true">
-                    <div class="modal-dialog modal-sm">
-                        <div class="modal-content">
-                            ...
-                            <!-- Form Book -->
-                
-                
-                        </div>
-                    </div>
-                </div>
+            <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto" data-src="" alt="Generic placeholder image">
+            </div>
+        </div>
+
+        <!-- Small modal -->
+        <button type="button" class="btn btn-primary col-sm-6" data-toggle="modal" data-target=".bd-example-modal-sm">Custom</button>
+
+        <div class="modal fade bd-example-modal-sm " tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-sm">
+                <div class="modal-content">
+                    ...
+                    <!-- Form Book -->
 
 
-        </section>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
 
 
     <!-- Others  End-->
 
     <hr class="featurette-divider">
-    
+
     <!-- FOOTER -->
     <footer class="container jumbotron col-md-12" style="margin: 20px 0 0 0;">
-     
+
         <p class="float-right"><a href="#">Top</a></p>
 
         <p class="text-center">&copy; Hactiv8 FWDB Final Assignment. </p>
     </footer>
 
- 
-    
 
-    
+
+
+
 </body>
+
 </html>
